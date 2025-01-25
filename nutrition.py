2d0bfb4230ede07094584c6f5be918e997c7e4db
MJ1717 <bananahamo77@gmail.com>
2025-01-25 13:25:53 -0500

    sdf

--- nutrition.py
+++ nutrition.py
@@ -4,16 +4,12 @@ from kivy.uix.label import Label
 
 kivy.require('1.9.0')
 
-class cal(App):
+class calculator(App):
 
     def build(self):
         return Label(text="nutrition calculator")
     
-call = cal()
-call.run()
+calculator1 = calculator()
+calculator1.run()
 
 
-#x
-#dfdfgdxdfgxfdxfg
-
-#dasdasdass
