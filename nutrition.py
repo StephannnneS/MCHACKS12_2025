0d5e78e1146629b37d61df0ce09467946eedaa3a
MJ1717 <bananahamo77@gmail.com>
2025-01-25 13:32:48 -0500

    asdasd

--- nutrition.py
+++ nutrition.py
@@ -1,13 +1,14 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 kivy.require('1.9.0')
 
 class calculator(App):
 
     def build(self):
-        return Label(text="nutrition calculator")
+        return BoxLayout()
     
 calculator1 = calculator()
 calculator1.run()
